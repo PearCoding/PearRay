6569e5736f82dd5a113feaed49162002b953cacd
pearcoding <pearcoding@gmail.com>
2016-04-30 15:18:13 +0200

    Fixed a tiny error.

--- src/tests/sampler.cpp
+++ src/tests/sampler.cpp
@@ -1,4 +1,4 @@
-#include "sampler/Sampler.h"
+#include "sampler/Projection.h"
 #include "Test.h"
 
 using namespace PR;
@@ -9,7 +9,7 @@ PR_TEST("Align 1");
 	auto N = PM::pm_Set(0, 0, 1);
 	auto V = PM::pm_Set(0, 1, 0);
 
-	auto R = Sampler::align(N, V);
+	auto R = Projection::align(N, V);
 
 	PR_CHECK_NEARLY_EQ_3(R, V);
 }
@@ -19,7 +19,7 @@ PR_TEST("Align 2");
 	auto N = PM::pm_Set(0, 1, 0);
 	auto V = PM::pm_Set(0, 1, 0);
 
-	auto R = Sampler::align(N, V);
+	auto R = Projection::align(N, V);
 
 	PR_CHECK_NEARLY_EQ_3(R, PM::pm_Set(0, 0, -1));
 }
@@ -29,7 +29,7 @@ PR_TEST("Align 3");
 	auto N = PM::pm_Normalize3D(PM::pm_Set(0, 0.5f, 0.5f));
 	auto V = PM::pm_Set(0, 1, 0);
 
-	auto R = Sampler::align(N, V);
+	auto R = Projection::align(N, V);
 
 	PR_CHECK_NEARLY_EQ_3(R, PM::pm_Normalize3D(PM::pm_Set(0, 0.5f, -0.5f)));
 }
@@ -39,7 +39,7 @@ PR_TEST("Align 4");
 	auto N = PM::pm_Set(1, 0, 0);
 	auto V = PM::pm_Set(0, 1, 0);
 
-	auto R = Sampler::align(N, V);
+	auto R = Projection::align(N, V);
 
 	PR_CHECK_NEARLY_EQ_3(R, PM::pm_Set(0, 1, 0));
 }
