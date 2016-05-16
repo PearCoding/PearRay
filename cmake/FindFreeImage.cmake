a51dc9140f9c2e31c618b9dbf73769cecd37b1d9
pearcoding <pearcoding@gmail.com>
2016-05-16 13:26:54 +0200

    Tiny fixes in cmake files.

--- cmake/FindFreeImage.cmake
+++ cmake/FindFreeImage.cmake
@@ -1,13 +1,11 @@
 # Locate Free Image
 # This module defines
 #  FREEIMAGE_FOUND, if false, do not try to link to FreeImage
-#  FREEIMAGE_LIBRARIES
-#  FREEIMAGE_INCLUDE_DIRS
+#  FREEIMAGE_LIBRARIY
+#  FREEIMAGE_INCLUDE_DIR
 #
 # Note that the expected include convention is
-#  #include "FreeImage.h"
-
-find_package(PackageHandleStandardArgs)
+#  #include <FreeImage.h>
 
 find_path(FREEIMAGE_INCLUDE_DIR FreeImage.h
   HINTS
@@ -36,8 +34,9 @@ find_library(FREEIMAGE_LIBRARY
   /opt
 )
 
+find_package(PackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(FreeImage
                                   REQUIRED_VARS FREEIMAGE_LIBRARY FREEIMAGE_INCLUDE_DIR)
 
-mark_as_advanced(FREEIMAGE_INCLUDE_DIR FREEIMAGE_LIBRARY LUA_MATH_LIBRARY)
+mark_as_advanced(FREEIMAGE_INCLUDE_DIR FREEIMAGE_LIBRARY)
 
