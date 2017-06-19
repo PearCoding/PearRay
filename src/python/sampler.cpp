6be65d1a8f21ec8057ecfbfd88930cc12ad7c6bb
PearCoding <pearcoding@gmail.com>
2017-06-19 22:18:13 +0200

    Added last pieces of the python API

--- src/python/sampler.cpp
+++ src/python/sampler.cpp
@@ -1,73 +1,96 @@
-#include <boost/python.hpp>
 #include "Random.h"
-#include "sampler/Sampler.h"
 #include "sampler/HaltonQMCSampler.h"
 #include "sampler/MultiJitteredSampler.h"
 #include "sampler/RandomSampler.h"
+#include "sampler/Sampler.h"
 #include "sampler/StratifiedSampler.h"
 #include "sampler/UniformSampler.h"
+#include <boost/python.hpp>
+
+#include "npmath.h"
 
 using namespace PR;
 namespace bpy = boost::python;
-namespace PRPY
+namespace np  = boost::python::numpy;
+
+namespace PRPY {
+class SamplerWrap : public Sampler, public bpy::wrapper<Sampler> {
+public:
+	inline float generate1D(uint32 index) override
+	{
+		return this->get_override("generate1D")(index);
+	}
+
+	inline Eigen::Vector2f generate2D(uint32 index) override
+	{
+		return vec2FromPython(generate2D_Py(index));
+	}
+
+	inline np::ndarray generate2D_Py(uint32 index)
+	{
+		return this->get_override("generate2D")(index);
+	}
+
+	inline Eigen::Vector3f generate3D(uint32 index) override
+	{
+		return vec3FromPython(generate3D_Py(index));
+	}
+
+	inline np::ndarray generate3D_Py(uint32 index)
+	{
+		return this->get_override("generate3D")(index);
+	}
+};
+
+class RandomWrap : public Random, public bpy::wrapper<Random> {
+public:
+	RandomWrap(uint64 seed)
+		: Random(seed)
+	{
+	}
+
+	np::ndarray get2D_Py()
+	{
+		return vec2ToPython(get2D());
+	}
+
+	np::ndarray get3D_Py()
+	{
+		return vec3ToPython(get3D());
+	}
+
+	np::ndarray get4D_Py()
+	{
+		return vec4ToPython(get4D());
+	}
+};
+
+void setup_sampler()
 {
-    class SamplerWrap : public Sampler, public bpy::wrapper<Sampler>
-    {
-    public:
-        inline float generate1D(uint32 index) override
-        {
-            return this->get_override("generate1D")(index);
-        }
+	bpy::class_<SamplerWrap, boost::noncopyable>("Sampler")
+		.def("generate1D", bpy::pure_virtual(&Sampler::generate1D))
+		.def("generate2D", bpy::pure_virtual(&SamplerWrap::generate2D_Py))
+		.def("generate3D", bpy::pure_virtual(&SamplerWrap::generate3D_Py));
 
-        inline Eigen::Vector2f generate2D(uint32 index) override
-        {
-            return this->get_override("generate2D")(index);
-        }
+	bpy::class_<HaltonQMCSampler, bpy::bases<Sampler>, boost::noncopyable>("HaltonQMCSampler", bpy::init<uint32, bpy::optional<bool, uint32, uint32, uint32>>());
 
-        inline Eigen::Vector3f generate3D(uint32 index) override
-        {
-            return this->get_override("generate3D")(index);
-        }
-    };
+	bpy::class_<MultiJitteredSampler, bpy::bases<Sampler>, boost::noncopyable>("MultiJitteredSampler", bpy::init<Random&, uint32>());
 
-    void setup_sampler()
-    {
-        bpy::class_<SamplerWrap, boost::noncopyable>("Sampler")
-            .def("generate1D", bpy::pure_virtual(&Sampler::generate1D))
-            .def("generate2D", bpy::pure_virtual(&Sampler::generate2D))
-            .def("generate3D", bpy::pure_virtual(&Sampler::generate3D))
-        ;
+	bpy::class_<RandomSampler, bpy::bases<Sampler>, boost::noncopyable>("RandomSampler", bpy::init<Random&>());
 
-        bpy::class_<HaltonQMCSampler, bpy::bases<Sampler>, boost::noncopyable>
-            ("HaltonQMCSampler", bpy::init<uint32, bpy::optional<bool, uint32, uint32, uint32>>())
-        ;
+	bpy::class_<StratifiedSampler, bpy::bases<Sampler>, boost::noncopyable>("StratifiedSampler", bpy::init<Random&, uint32>());
 
-        bpy::class_<MultiJitteredSampler, bpy::bases<Sampler>, boost::noncopyable>
-            ("MultiJitteredSampler", bpy::init<Random&, uint32>())
-        ;
+	bpy::class_<UniformSampler, bpy::bases<Sampler>, boost::noncopyable>("UniformSampler", bpy::init<Random&, uint32>());
 
-        bpy::class_<RandomSampler, bpy::bases<Sampler>, boost::noncopyable>
-            ("RandomSampler", bpy::init<Random&>())
-        ;
-
-        bpy::class_<StratifiedSampler, bpy::bases<Sampler>, boost::noncopyable>
-            ("StratifiedSampler", bpy::init<Random&, uint32>())
-        ;
-
-        bpy::class_<UniformSampler, bpy::bases<Sampler>, boost::noncopyable>
-            ("UniformSampler", bpy::init<Random&, uint32>())
-        ;
-
-        bpy::class_<Random, boost::noncopyable>("Random", bpy::init<uint64 >())
-            .def("get32", (uint32_t(Random::*)())&Random::get32)
-            .def("get32", (uint32_t(Random::*)(uint32_t, uint32_t))&Random::get32)
-            .def("get64", (uint64_t(Random::*)())&Random::get64)
-            .def("get64", (uint64_t(Random::*)(uint64_t, uint64_t))&Random::get64)
-            .def("getFloat", &Random::getFloat)
-            .def("getDouble", &Random::getDouble)
-            .def("get2D", &Random::get2D)
-            .def("get3D", &Random::get3D)
-            .def("get4D", &Random::get4D)
-        ;
-    }
+	bpy::class_<RandomWrap, boost::noncopyable>("Random", bpy::init<uint64>())
+		.def("get32", (uint32_t(Random::*)()) & Random::get32)
+		.def("get32", (uint32_t(Random::*)(uint32_t, uint32_t)) & Random::get32)
+		.def("get64", (uint64_t(Random::*)()) & Random::get64)
+		.def("get64", (uint64_t(Random::*)(uint64_t, uint64_t)) & Random::get64)
+		.def("getFloat", &Random::getFloat)
+		.def("getDouble", &Random::getDouble)
+		.def("get2D", &RandomWrap::get2D_Py)
+		.def("get3D", &RandomWrap::get3D_Py)
+		.def("get4D", &RandomWrap::get4D_Py);
+}
 }
--- src/python/shader.cpp
+++ src/python/shader.cpp
@@ -1,8 +1,8 @@
-#include <boost/python.hpp>
 #include "material/Material.h"
 #include "shader/FaceSample.h"
 #include "shader/ShaderClosure.h"
 #include "shader/ShaderOutput.h"
+#include <boost/python.hpp>
 
 // Implementations
 #include "shader/ConstScalarOutput.h"
@@ -12,151 +12,168 @@
 #include "shader/ImageSpectralOutput.h"
 #include "shader/ImageVectorOutput.h"
 
+#include "npmath.h"
+
 using namespace PR;
 namespace bpy = boost::python;
-namespace PRPY
+namespace np  = boost::python::numpy;
+
+namespace PRPY {
+#define ATTR(name)                                                           \
+	inline np::ndarray get##name##_Py() const { return vec3ToPython(name); } \
+	inline void set##name##_Py(const np::ndarray& v) { name = vec3FromPython(v); }
+
+#define PROB_FS(name) \
+	.add_property(PR_STRINGIFY(name), &FSWrap::get##name##_Py, &FSWrap::set##name##_Py)
+
+#define PROB_SC(name) \
+	.add_property(PR_STRINGIFY(name), &SCWrap::get##name##_Py, &SCWrap::set##name##_Py)
+
+class FSWrap : public FaceSample, public bpy::wrapper<FaceSample> {
+public:
+	FSWrap()
+		: FaceSample()
+	{
+	}
+	FSWrap(const boost::reference_wrapper<const FaceSample>::type& other)
+		: FaceSample(other)
+	{
+	}
+
+	ATTR(P)
+	ATTR(dPdX) ATTR(dPdY) ATTR(dPdZ)
+		ATTR(dPdU) ATTR(dPdV) ATTR(dPdW) ATTR(dPdT)
+			ATTR(Ng) ATTR(Nx) ATTR(Ny)
+				ATTR(UVW) ATTR(dUVWdX) ATTR(dUVWdY) ATTR(dUVWdZ)
+
+					inline PR::Material* material_Py() const
+	{
+		return Material;
+	}
+	inline void setMaterial_Py(PR::Material* m) { Material = m; }
+};
+
+class SCWrap : public ShaderClosure, public bpy::wrapper<ShaderClosure> {
+public:
+	SCWrap()
+		: ShaderClosure()
+	{
+	}
+	SCWrap(const boost::reference_wrapper<const ShaderClosure>::type& other)
+		: ShaderClosure(other)
+	{
+	}
+
+	ATTR(P)
+	ATTR(dPdX) ATTR(dPdY) ATTR(dPdZ)
+		ATTR(dPdU) ATTR(dPdV) ATTR(dPdW) ATTR(dPdT)
+			ATTR(V) ATTR(dVdX) ATTR(dVdY)
+				ATTR(N) ATTR(Ng) ATTR(Nx) ATTR(Ny)
+					ATTR(UVW) ATTR(dUVWdX) ATTR(dUVWdY) ATTR(dUVWdZ)
+
+						inline PR::Material* material_Py() const
+	{
+		return Material;
+	}
+	inline void setMaterial_Py(PR::Material* m) { Material = m; }
+};
+
+class ScalarShaderOutputWrap : public ScalarShaderOutput, public bpy::wrapper<ScalarShaderOutput> {
+public:
+	float eval(const ShaderClosure& point) override
+	{
+		return this->get_override("eval")(point);
+	}
+};
+
+class SpectralShaderOutputWrap : public SpectralShaderOutput, public bpy::wrapper<SpectralShaderOutput> {
+public:
+	Spectrum eval(const ShaderClosure& point) override
+	{
+		return this->get_override("eval")(point);
+	}
+};
+
+class VectorShaderOutputWrap : public VectorShaderOutput, public bpy::wrapper<VectorShaderOutput> {
+public:
+	Eigen::Vector3f eval(const ShaderClosure& point) override
+	{
+		return vec3FromPython(eval_Py(point));
+	}
+
+	np::ndarray eval_Py(const ShaderClosure& point)
+	{
+		return this->get_override("eval")(point);
+	}
+};
+
+class ConstVectorShaderOutputWrap : public ConstVectorShaderOutput, public bpy::wrapper<ConstVectorShaderOutput> {
+public:
+	ConstVectorShaderOutputWrap(const np::ndarray& array)
+		: ConstVectorShaderOutput(vec3FromPython(array))
+	{
+	}
+};
+
+void setup_shader()
 {
-    #define ATTR(name) \
-        inline Eigen::Vector3f get##name##_Py() const { return name; } \
-        inline void set##name##_Py(const Eigen::Vector3f& v) { name = v; }
-    
-    #define PROB_FS(name) \
-        .add_property(PR_STRINGIFY(name), &FSWrap::get##name##_Py, &FSWrap::set##name##_Py)
+	bpy::class_<FSWrap>("FaceSample")
+		PROB_FS(P) PROB_FS(dPdX) PROB_FS(dPdY) PROB_FS(dPdZ)
+			PROB_FS(dPdU) PROB_FS(dPdV) PROB_FS(dPdW) PROB_FS(dPdT)
+				PROB_FS(Ng) PROB_FS(Nx) PROB_FS(Ny)
+					PROB_FS(UVW) PROB_FS(dUVWdX) PROB_FS(dUVWdY) PROB_FS(dUVWdZ)
+						.add_property("Material",
+									  bpy::make_function(&FSWrap::material_Py, bpy::return_internal_reference<>()),
+									  &FSWrap::setMaterial_Py);
 
-    #define PROB_SC(name) \
-        .add_property(PR_STRINGIFY(name), &SCWrap::get##name##_Py, &SCWrap::set##name##_Py)
+	bpy::class_<SCWrap>("ShaderClosure")
+		PROB_SC(P) PROB_SC(dPdX) PROB_SC(dPdY) PROB_SC(dPdZ)
+			PROB_SC(dPdU) PROB_SC(dPdV) PROB_SC(dPdW) PROB_SC(dPdT)
+				PROB_SC(V) PROB_SC(dVdX) PROB_SC(dVdY)
+					PROB_SC(N) PROB_SC(Ng) PROB_SC(Nx) PROB_SC(Ny)
+						PROB_SC(UVW) PROB_SC(dUVWdX) PROB_SC(dUVWdY) PROB_SC(dUVWdZ)
+							.def_readwrite("T", &ShaderClosure::T)
+							.def_readwrite("WavelengthIndex", &ShaderClosure::WavelengthIndex)
+							.def_readwrite("Depth2", &ShaderClosure::Depth2)
+							.def_readwrite("NgdotV", &ShaderClosure::NgdotV)
+							.def_readwrite("NdotV", &ShaderClosure::NdotV)
+							.def_readwrite("Flags", &ShaderClosure::Flags)
+							.def_readwrite("EntityID", &ShaderClosure::EntityID)
+							.add_property("Material",
+										  bpy::make_function(&SCWrap::material_Py, bpy::return_internal_reference<>()),
+										  &SCWrap::setMaterial_Py);
 
-    class FSWrap : public FaceSample, public bpy::wrapper<FaceSample>
-    {
-    public:
-        FSWrap() : FaceSample() {}
-        FSWrap(const boost::reference_wrapper<const FaceSample>::type& other) :
-            FaceSample(other) {}
+	bpy::enum_<ShaderClosureFlags>("ShaderClosureFlags")
+		.value("INSIDE", SCF_Inside);
 
-        ATTR(P) ATTR(dPdX) ATTR(dPdY) ATTR(dPdZ)
-        ATTR(dPdU) ATTR(dPdV) ATTR(dPdW) ATTR(dPdT)
-        ATTR(Ng) ATTR(Nx) ATTR(Ny)
-        ATTR(UVW) ATTR(dUVWdX) ATTR(dUVWdY) ATTR(dUVWdZ)
+	bpy::class_<ScalarShaderOutputWrap, std::shared_ptr<ScalarShaderOutputWrap>, boost::noncopyable>("ScalarShaderOutput")
+		.def("eval", bpy::pure_virtual(&ScalarShaderOutput::eval));
 
-        inline PR::Material* material_Py() const { return Material; }
-        inline void setMaterial_Py(PR::Material* m) { Material = m; }
-    };
+	bpy::class_<SpectralShaderOutputWrap, std::shared_ptr<SpectralShaderOutputWrap>, boost::noncopyable>("SpectralShaderOutput")
+		.def("eval", bpy::pure_virtual(&SpectralShaderOutput::eval));
 
-    class SCWrap : public ShaderClosure, public bpy::wrapper<ShaderClosure>
-    {
-    public:
-        SCWrap() : ShaderClosure() {}
-        SCWrap(const boost::reference_wrapper<const ShaderClosure>::type& other) :
-            ShaderClosure(other) {}
+	bpy::class_<VectorShaderOutputWrap, std::shared_ptr<VectorShaderOutputWrap>, boost::noncopyable>("VectorShaderOutput")
+		.def("eval", bpy::pure_virtual(&VectorShaderOutputWrap::eval_Py));
 
-        ATTR(P) ATTR(dPdX) ATTR(dPdY) ATTR(dPdZ)
-        ATTR(dPdU) ATTR(dPdV) ATTR(dPdW) ATTR(dPdT)
-        ATTR(V) ATTR(dVdX) ATTR(dVdY)
-        ATTR(N) ATTR(Ng) ATTR(Nx) ATTR(Ny)
-        ATTR(UVW) ATTR(dUVWdX) ATTR(dUVWdY) ATTR(dUVWdZ)
+	bpy::register_ptr_to_python<std::shared_ptr<ScalarShaderOutput>>();
+	bpy::register_ptr_to_python<std::shared_ptr<SpectralShaderOutput>>();
+	bpy::register_ptr_to_python<std::shared_ptr<VectorShaderOutput>>();
 
-        inline PR::Material* material_Py() const { return Material; }
-        inline void setMaterial_Py(PR::Material* m) { Material = m; }
-    };
-
-    class ScalarShaderOutputWrap : public ScalarShaderOutput, public bpy::wrapper<ScalarShaderOutput>
-    {
-    public:
-        float eval(const ShaderClosure& point) override
-        {
-            return this->get_override("eval")(point);
-        }
-    };
-
-    class SpectralShaderOutputWrap : public SpectralShaderOutput, public bpy::wrapper<SpectralShaderOutput>
-    {
-    public:
-        Spectrum eval(const ShaderClosure& point) override
-        {
-            return this->get_override("eval")(point);
-        }
-    };
-
-    class VectorShaderOutputWrap : public VectorShaderOutput, public bpy::wrapper<VectorShaderOutput>
-    {
-    public:
-        Eigen::Vector3f eval(const ShaderClosure& point) override
-        {
-            return this->get_override("eval")(point);
-        }
-    };
-
-    void setup_shader()
-    {
-        bpy::class_<FSWrap>("FaceSample")
-        PROB_FS(P) PROB_FS(dPdX) PROB_FS(dPdY) PROB_FS(dPdZ)
-        PROB_FS(dPdU) PROB_FS(dPdV) PROB_FS(dPdW) PROB_FS(dPdT)
-        PROB_FS(Ng) PROB_FS(Nx) PROB_FS(Ny)
-        PROB_FS(UVW) PROB_FS(dUVWdX) PROB_FS(dUVWdY) PROB_FS(dUVWdZ)
-        .add_property("Material",
-             bpy::make_function(&FSWrap::material_Py, bpy::return_internal_reference<>()),
-             &FSWrap::setMaterial_Py)
-        ;
-
-        bpy::class_<SCWrap>("ShaderClosure")
-        PROB_SC(P) PROB_SC(dPdX) PROB_SC(dPdY) PROB_SC(dPdZ)
-        PROB_SC(dPdU) PROB_SC(dPdV) PROB_SC(dPdW) PROB_SC(dPdT)
-        PROB_SC(V) PROB_SC(dVdX) PROB_SC(dVdY)
-        PROB_SC(N) PROB_SC(Ng) PROB_SC(Nx) PROB_SC(Ny)
-        PROB_SC(UVW) PROB_SC(dUVWdX) PROB_SC(dUVWdY) PROB_SC(dUVWdZ)
-        .def_readwrite("T", &ShaderClosure::T)
-        .def_readwrite("WavelengthIndex", &ShaderClosure::WavelengthIndex)
-        .def_readwrite("Depth2", &ShaderClosure::Depth2)
-        .def_readwrite("NgdotV", &ShaderClosure::NgdotV)
-        .def_readwrite("NdotV", &ShaderClosure::NdotV)
-        .def_readwrite("Flags", &ShaderClosure::Flags)
-        .def_readwrite("EntityID", &ShaderClosure::EntityID)
-        .add_property("Material",
-             bpy::make_function(&SCWrap::material_Py, bpy::return_internal_reference<>()),
-             &SCWrap::setMaterial_Py)
-        ;
-
-        bpy::enum_<ShaderClosureFlags>("ShaderClosureFlags")
-        .value("INSIDE", SCF_Inside)
-        ;
-
-        bpy::class_<ScalarShaderOutputWrap, std::shared_ptr<ScalarShaderOutputWrap>, boost::noncopyable>("ScalarShaderOutput")
-        .def("eval", bpy::pure_virtual(&ScalarShaderOutput::eval))
-        ;
-
-        bpy::class_<SpectralShaderOutputWrap, std::shared_ptr<SpectralShaderOutputWrap>, boost::noncopyable>("SpectralShaderOutput")
-        .def("eval", bpy::pure_virtual(&SpectralShaderOutput::eval))
-        ;
-
-        bpy::class_<VectorShaderOutputWrap, std::shared_ptr<VectorShaderOutputWrap>, boost::noncopyable>("VectorShaderOutput")
-        .def("eval", bpy::pure_virtual(&VectorShaderOutputWrap::eval))
-        ;
-
-        bpy::register_ptr_to_python<std::shared_ptr<ScalarShaderOutput> >();
-        bpy::register_ptr_to_python<std::shared_ptr<SpectralShaderOutput> >();
-        bpy::register_ptr_to_python<std::shared_ptr<VectorShaderOutput> >();
-
-        // Implementations
-        bpy::class_<ConstScalarShaderOutput, std::shared_ptr<ConstScalarShaderOutput>, bpy::bases<ScalarShaderOutput>, boost::noncopyable >
-            ("ConstScalarShaderOutput", bpy::init<float>())
-        ;
-        bpy::implicitly_convertible<std::shared_ptr<ConstScalarShaderOutput>, std::shared_ptr<ScalarShaderOutput> >();
-        bpy::class_<ConstSpectralShaderOutput, std::shared_ptr<ConstSpectralShaderOutput>, bpy::bases<SpectralShaderOutput>, boost::noncopyable >
-            ("ConstSpectralShaderOutput", bpy::init<const PR::Spectrum&>())
-        ;
-        bpy::implicitly_convertible<std::shared_ptr<ConstSpectralShaderOutput>, std::shared_ptr<SpectralShaderOutput> >();
-        bpy::class_<ConstVectorShaderOutput, std::shared_ptr<ConstVectorShaderOutput>, bpy::bases<VectorShaderOutput>, boost::noncopyable >
-            ("ConstVectorShaderOutput", bpy::init<const Eigen::Vector3f&>())
-        ;
-        bpy::implicitly_convertible<std::shared_ptr<ConstVectorShaderOutput>, std::shared_ptr<VectorShaderOutput> >();
-        // bpy::class_<ImageScalarOutput, std::shared_ptr<ImageScalarOutput>, bpy::bases<ScalarShaderOutput>, boost::noncopyable >
-        //     ("ImageScalarOutput", bpy::init<float>())
-        // ;
-        // bpy::class_<ImageSpectralOutput, std::shared_ptr<ImageSpectralOutput>, bpy::bases<SpectralShaderOutput>, boost::noncopyable >
-        //     ("ImageSpectralOutput", bpy::init<const PR::Spectrum&>())
-        // ;
-        // bpy::class_<ImageVectorOutput, std::shared_ptr<ImageVectorOutput>, bpy::bases<VectorShaderOutput>, boost::noncopyable >
-        //     ("ImageVectorOutput", bpy::init<const Eigen::Vector3f&>())
-        // ;
-    }
+	// Implementations
+	bpy::class_<ConstScalarShaderOutput, std::shared_ptr<ConstScalarShaderOutput>, bpy::bases<ScalarShaderOutput>, boost::noncopyable>("ConstScalarShaderOutput", bpy::init<float>());
+	bpy::implicitly_convertible<std::shared_ptr<ConstScalarShaderOutput>, std::shared_ptr<ScalarShaderOutput>>();
+	bpy::class_<ConstSpectralShaderOutput, std::shared_ptr<ConstSpectralShaderOutput>, bpy::bases<SpectralShaderOutput>, boost::noncopyable>("ConstSpectralShaderOutput", bpy::init<const PR::Spectrum&>());
+	bpy::implicitly_convertible<std::shared_ptr<ConstSpectralShaderOutput>, std::shared_ptr<SpectralShaderOutput>>();
+	bpy::class_<ConstVectorShaderOutputWrap, std::shared_ptr<ConstVectorShaderOutputWrap>, bpy::bases<VectorShaderOutput>, boost::noncopyable>("ConstVectorShaderOutput", bpy::init<const np::ndarray&>());
+	bpy::implicitly_convertible<std::shared_ptr<ConstVectorShaderOutputWrap>, std::shared_ptr<VectorShaderOutput>>();
+	// bpy::class_<ImageScalarOutput, std::shared_ptr<ImageScalarOutput>, bpy::bases<ScalarShaderOutput>, boost::noncopyable >
+	//     ("ImageScalarOutput", bpy::init<float>())
+	// ;
+	// bpy::class_<ImageSpectralOutput, std::shared_ptr<ImageSpectralOutput>, bpy::bases<SpectralShaderOutput>, boost::noncopyable >
+	//     ("ImageSpectralOutput", bpy::init<const PR::Spectrum&>())
+	// ;
+	// bpy::class_<ImageVectorOutput, std::shared_ptr<ImageVectorOutput>, bpy::bases<VectorShaderOutput>, boost::noncopyable >
+	//     ("ImageVectorOutput", bpy::init<const Eigen::Vector3f&>())
+	// ;
+}
 }
