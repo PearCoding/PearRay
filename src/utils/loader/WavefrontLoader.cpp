2da498c31284e354645b975390615d93b0c42968
PearCoding <pearcoding@gmail.com>
2018-02-19 00:50:26 +0100

    Updated submodules....

--- src/utils/loader/WavefrontLoader.cpp
+++ src/utils/loader/WavefrontLoader.cpp
@@ -6,7 +6,7 @@
 #include "Logger.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION
-#include "external/tiny_obj_loader.h"
+#include "tiny_obj_loader.h"
 
 namespace PR {
 WavefrontLoader::WavefrontLoader(const std::map<std::string, std::string>& overrides)
@@ -22,47 +22,44 @@ WavefrontLoader::~WavefrontLoader()
 
 void WavefrontLoader::load(const std::string& file, Environment* env)
 {
+	tinyobj::attrib_t attrib;
 	std::vector<tinyobj::shape_t> shapes;
 	std::vector<tinyobj::material_t> materials; // We ignore materials for now
 	std::string err;
 
-	if (!tinyobj::LoadObj(shapes, materials, err, file.c_str())) {
+	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, file.c_str())) {
 		PR_LOGGER.instance().logf(L_Error, M_Scene, "Error reading Wavefront file: %s", err.c_str());
 	} else {
+		if (!err.empty()) {
+			PR_LOGGER.instance().logf(L_Warning, M_Scene, "Wavefront file: %s", err.c_str());
+		}
+
 		for (tinyobj::shape_t shape : shapes) {
 			auto mesh = std::make_shared<PR::TriMesh>();
 			mesh->reserve(shape.mesh.indices.size() / 3);
 
-			bool hasNorm = shape.mesh.normals.size() > 0;
-			bool hasUV   = shape.mesh.texcoords.size() > 0;
-			float inv	= mFlipNormal ? -1.0f : 1.0f;
+			float inv = mFlipNormal ? -1.0f : 1.0f;
 
 			for (size_t i = 0; i < shape.mesh.indices.size() / 3; ++i) {
 				Face* face = new Face;
 				for (int j = 0; j < 3; j++) {
-					int idx	= shape.mesh.indices[3 * i + j];
-					face->V[j] = Eigen::Vector3f(mScale * shape.mesh.positions[3 * idx],
-												 mScale * shape.mesh.positions[3 * idx + 1],
-												 mScale * shape.mesh.positions[3 * idx + 2]);
+					tinyobj::index_t idx = shape.mesh.indices[3 * i + j];
 
-					if (hasNorm) {
-						face->N[j] = Eigen::Vector3f(inv * shape.mesh.normals[3 * idx],
-													 inv * shape.mesh.normals[3 * idx + 1],
-													 inv * shape.mesh.normals[3 * idx + 2])
-										 .normalized();
-					}
+					face->V[j] = Eigen::Vector3f(mScale * attrib.vertices[3 * idx.vertex_index + 0],
+												 mScale * attrib.vertices[3 * idx.vertex_index + 1],
+												 mScale * attrib.vertices[3 * idx.vertex_index + 2]);
 
-					if (hasUV) {
-						face->UV[j] = Eigen::Vector2f(shape.mesh.texcoords[2 * idx],
-													  shape.mesh.texcoords[2 * idx + 1]);
-					}
+					face->N[j] = Eigen::Vector3f(inv * attrib.normals[3 * idx.normal_index + 0],
+												 inv * attrib.normals[3 * idx.normal_index + 1],
+												 inv * attrib.normals[3 * idx.normal_index + 2])
+									 .normalized();
+
+					face->UV[j] = Eigen::Vector2f(attrib.texcoords[2 * idx.texcoord_index + 0],
+												  attrib.texcoords[2 * idx.texcoord_index + 1]);
 				}
 
 				mesh->addFace(face);
 			}
-
-			if (!hasNorm)
-				mesh->calcNormals();
 
 			mesh->build();
 
