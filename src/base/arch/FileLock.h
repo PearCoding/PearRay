642aa9698704f48d8428be962ccd881bf9f648f9
PearCoding <pearcoding@gmail.com>
2020-05-15 14:13:02 +0200

    Documentation update

--- src/base/arch/FileLock.h
+++ src/base/arch/FileLock.h
@@ -3,6 +3,7 @@
 #include "PR_Config.h"
 
 namespace PR {
+/// Locked file to ensure only one raytracers works on one scene
 class PR_LIB_BASE FileLock final {
 public:
 	FileLock(const std::wstring& filepath);
