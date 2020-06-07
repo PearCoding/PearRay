77ba4b30ef98b1c1fbb468e19a3645463c4ce8af
PearCoding <pearcoding@gmail.com>
2020-06-08 00:16:12 +0200

    Added PCG random generators

--- cmake/Git.cmake
+++ cmake/Git.cmake
@@ -1,6 +1,13 @@
+find_package(Git QUIET)
+if(NOT GIT_FOUND OR NOT EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    set(GIT_BRANCH "HEAD")
+    set(GIT_REVISION "")
+    return()
+endif()
+
 # Get the current working branch
 execute_process(
-COMMAND git rev-parse --abbrev-ref HEAD
+COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 OUTPUT_VARIABLE GIT_BRANCH
 OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -8,8 +15,25 @@ OUTPUT_STRIP_TRAILING_WHITESPACE
 
 # Get the latest abbreviated commit hash of the working branch
 execute_process(
-COMMAND git log -1 --format=%h
+COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 OUTPUT_VARIABLE GIT_REVISION
 OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+
+# Update submodules as needed
+option(GIT_SUBMODULE "Check submodules during build" ON)
+if(GIT_SUBMODULE)
+    message(STATUS "Submodule update")
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    RESULT_VARIABLE GIT_SUBMOD_RESULT)
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+        message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+    endif()
+endif()
+
+# Check at least one of the external modules if everything is fine
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/external/DataLisp/CMakeLists.txt")
+    message(FATAL_ERROR "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+endif()
--- src/core/Random.h
+++ src/core/Random.h
@@ -6,15 +6,20 @@
  * Available random algorithms
  * 0 - STL
  * 1 - Mult based
- * 2 - Xorshift RNGs (George Marsaglia) [Default]
+ * 2 - Xorshift RNGs (George Marsaglia) 
+ * 3 - PCG [Default]
  */
 
 #ifndef PR_RANDOM_ALGORITHM
-#define PR_RANDOM_ALGORITHM (2)
+#define PR_RANDOM_ALGORITHM (3)
 #endif
 
-#if PR_RANDOM_ALGORITHM == 0
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
 #include <random>
+#endif
+
+#if PR_RANDOM_ALGORITHM == 3
+#include "pcg_random.hpp"
 #endif
 
 namespace PR {
@@ -32,8 +37,16 @@ private:
 	const uint64 mSeed;
 
 	static constexpr uint64 MULT = 0x5851f42d4c957f2dULL; //6364136223846793005ULL
-#else
+#elif PR_RANDOM_ALGORITHM == 2
 	uint64 mState;
+#elif PR_RANDOM_ALGORITHM == 3
+	pcg32_fast mGenerator;
+	std::uniform_real_distribution<float> mDistributionFloat;
+	std::uniform_real_distribution<double> mDistributionDouble;
+	std::uniform_int_distribution<uint32> mDistributionUInt32;
+	std::uniform_int_distribution<uint64> mDistributionUInt64;
+#else
+#error Unknown random algorithm
 #endif
 
 public:
@@ -41,16 +54,22 @@ public:
 		:
 #if PR_RANDOM_ALGORITHM == 0
 		mGenerator(seed)
-		, mDistributionFloat(0.0f, 1.0f)
-		, mDistributionDouble(0.0, 1.0)
+		, mDistributionFloat()	//[0,1)
+		, mDistributionDouble() //[0,1)
 		, mDistributionUInt32()
 		, mDistributionUInt64()
 #elif PR_RANDOM_ALGORITHM == 1
 		mState(0x64326ae2f48fe6dbULL)
 		, mInc(0xf13e39cbe9a35bdbULL)
 		, mSeed(seed)
-#else
+#elif PR_RANDOM_ALGORITHM == 2
 		mState(seed)
+#elif PR_RANDOM_ALGORITHM == 3
+		mGenerator(seed)
+		, mDistributionFloat()	//[0,1)
+		, mDistributionDouble() //[0,1)
+		, mDistributionUInt32()
+		, mDistributionUInt64()
 #endif
 	{
 	}
@@ -67,7 +86,7 @@ public:
 	// Standard interface
 	inline uint32 get32()
 	{
-#if PR_RANDOM_ALGORITHM == 0
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
 		return mDistributionUInt32(mGenerator);
 #elif PR_RANDOM_ALGORITHM == 1
 		uint64 oldstate = mState;
@@ -80,57 +99,57 @@ public:
 		uint32 rot		  = static_cast<uint32>(oldstate >> 59u);
 
 		return ((xorshifted >> rot) | (xorshifted << ((~rot + 1u) & 31))) ^ static_cast<uint32>(mSeed);
-#else
+#elif PR_RANDOM_ALGORITHM == 2
 		return static_cast<uint32>(get64() & 0xFFFFFFFF);
 #endif
 	}
 
-	inline uint32 get32(uint32 start, uint32 end)
+	inline uint32 get32(uint32 start, uint32 end) // [start, end-1]
 	{
-#if PR_RANDOM_ALGORITHM == 0
-		return std::uniform_int_distribution<uint32>(start, end)(mGenerator);
-#else
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
+		return std::uniform_int_distribution<uint32>(start, end - 1)(mGenerator);
+#elif PR_RANDOM_ALGORITHM == 1 || PR_RANDOM_ALGORITHM == 2
 		return (get32() % (end - start)) + start;
 #endif
 	}
 
 	inline uint64 get64()
 	{
-#if PR_RANDOM_ALGORITHM == 0
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
 		return mDistributionUInt64(mGenerator);
 #elif PR_RANDOM_ALGORITHM == 1
 		return ((static_cast<uint64>(get32())) << 32) | static_cast<uint64>(get32());
-#else
+#elif PR_RANDOM_ALGORITHM == 2
 		mState ^= (mState << 13);
 		mState ^= (mState >> 7);
 		return (mState ^= (mState << 17));
 #endif
 	}
 
-	inline uint64 get64(uint64 start, uint64 end)
+	inline uint64 get64(uint64 start, uint64 end) // [start, end-1]
 	{
-#if PR_RANDOM_ALGORITHM == 0
-		return std::uniform_int_distribution<uint64>(start, end)(mGenerator);
-#else
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
+		return std::uniform_int_distribution<uint64>(start, end - 1)(mGenerator);
+#elif PR_RANDOM_ALGORITHM == 1 || PR_RANDOM_ALGORITHM == 2
 		return (get64() % (end - start)) + start;
 #endif
 	}
 
-	inline float getFloat() //[0, 1]
+	inline float getFloat() //[0, 1)
 	{
-#if PR_RANDOM_ALGORITHM == 0
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
 		return mDistributionFloat(mGenerator);
-#else
+#elif PR_RANDOM_ALGORITHM == 1 || PR_RANDOM_ALGORITHM == 2
 		//return get32() * 2.328306436538696e-10f;
 		return get32() / static_cast<float>(std::numeric_limits<uint32>::max());
 #endif
 	}
 
-	inline double getDouble() //[0, 1]
+	inline double getDouble() //[0, 1)
 	{
-#if PR_RANDOM_ALGORITHM == 0
+#if PR_RANDOM_ALGORITHM == 0 || PR_RANDOM_ALGORITHM == 3
 		return mDistributionDouble(mGenerator);
-#else
+#elif PR_RANDOM_ALGORITHM == 1 || PR_RANDOM_ALGORITHM == 2
 		return get64() / static_cast<double>(std::numeric_limits<uint64>::max());
 #endif
 	}
@@ -148,24 +167,6 @@ public:
 	inline Vector4f get4D()
 	{
 		return Vector4f(getFloat(), getFloat(), getFloat(), getFloat());
-	}
-
-	inline vuint32 get32v()
-	{
-		PR_SIMD_ALIGN uint32 r[PR_SIMD_BANDWIDTH];
-		for (size_t i = 0; i < PR_SIMD_BANDWIDTH; ++i)
-			r[i] = get32();
-
-		return simdpp::load(r);
-	}
-
-	inline vfloat getFloatv()
-	{
-		PR_SIMD_ALIGN float r[PR_SIMD_BANDWIDTH];
-		for (size_t i = 0; i < PR_SIMD_BANDWIDTH; ++i)
-			r[i] = getFloat();
-
-		return simdpp::load(r);
 	}
 };
 } // namespace PR
