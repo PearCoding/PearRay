593203798b83cbcad98e281da0e9bfa9f06e34db
PearCoding <pearcoding@gmail.com>
2020-06-14 22:19:51 +0200

    Removed obsolete files

--- src/core/geometry/TriangleIntersection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "TriangleIntersection_BW9.h"
-#include "TriangleIntersection_BW12.h"
-#include "TriangleIntersection_MT.h"
-#include "TriangleIntersection_PI.h"
-#include "TriangleIntersection_PI_Mem.h"
-#include "TriangleIntersection_PI_Off.h"
-#include "TriangleIntersection_WT.h"
