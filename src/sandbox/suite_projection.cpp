7a4336d5e4378cb258400acdff7875d8fa8163e4
PearCoding <pearcoding@gmail.com>
2020-08-17 12:56:42 +0200

    Fixed build error

--- src/sandbox/suite_projection.cpp
+++ src/sandbox/suite_projection.cpp
@@ -10,28 +10,24 @@ namespace sf = std::filesystem;
 
 void suite_projection()
 {
-    sf::create_directory("results/projection");
+	sf::create_directory("results/projection");
 
-    std::ofstream outfile("results/projection/cos_hemi.cvs");
+	std::ofstream outfile("results/projection/cos_hemi.cvs");
 
-    constexpr float step=0.01f;
+	constexpr float step = 0.01f;
 
-    constexpr int N = 1/step + 1;
-    for(int i = 0; i < N; ++i)
-    {
-        for(int j = 0; j < N; ++j)
-        {
-            const float u1 = i*step;
-            const float u2 = j*step;
+	constexpr int N = 1 / step + 1;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			const float u1 = i * step;
+			const float u2 = j * step;
 
-            outfile << u1 << " " << u2;
-            for(int k = 0; k < 10; ++k)
-            {
-                float pdf;
-                Sampling::cos_hemi(u1,u2,k,pdf);
-                outfile << " " << pdf;
-            }
-            outfile << std::endl;
-        }
-    }
+			outfile << u1 << " " << u2;
+			for (int k = 0; k < 10; ++k) {
+				const Vector3f dir = Sampling::cos_hemi(u1, u2, k);
+				outfile << " " << Sampling::cos_hemi_pdf(dir(2));
+			}
+			outfile << std::endl;
+		}
+	}
 }
