f11eb4495c721ff58b1b995eabec05048c6ea6d6
Ömercan Yazici <yazici@cg.uni-saarland.de>
2020-09-15 15:45:41 +0200

    Fixed correlated multi jittering

--- src/plugins/main/sampler/MultiJitteredSampler.cpp
+++ src/plugins/main/sampler/MultiJitteredSampler.cpp
@@ -8,6 +8,7 @@
 
 namespace PR {
 #define PR_MJS_USE_RANDOM
+#define PR_MJS_CLIP
 
 /*
 Reference:
@@ -91,55 +92,69 @@ static inline float randfloat(uint32 i, uint32 p)
 
 class MultiJitteredSampler : public ISampler {
 public:
-	MultiJitteredSampler(Random& random, uint32 samples)
+	MultiJitteredSampler(Random& random, uint32 samples, uint32 bins, uint32 seed)
 		: ISampler(samples)
 		, mRandom(random)
-		, m2D_X(static_cast<uint32>(std::sqrt(samples)))
-		, m2D_Y((samples + m2D_X - 1) / m2D_X)
+		, m1D(bins)
+		, m2D_X(static_cast<uint32>(std::sqrt(bins)))
+		, m2D_Y((bins + m2D_X - 1) / m2D_X)
+		, mSeed(seed)
 	{
 	}
 
 	float generate1D(uint32 index) override
 	{
-		//uint32 s = permute(index % maxSamples(), maxSamples(), p * 0xa399d265);
-
 #ifndef PR_MJS_USE_RANDOM
-		uint32 p = mRandom.get32();
-		float j  = randfloat(index % maxSamples(), p * 0x711ad6a5);
+		const float j = randfloat(index % m1D, mSeed * 0x711ad6a5);
 #else
-		float j  = mRandom.getFloat();
+		const float j		= mRandom.getFloat();
 #endif
 
-		float r = (index % maxSamples() + j) / maxSamples();
+		const float r = (index % m1D + j) / m1D;
 		return r;
 	}
 
 	Vector2f generate2D(uint32 index) override
 	{
-		uint32 newIndex = index % maxSamples();
-
-		uint32 p  = mRandom.get32();
-		uint32 sx = permute(newIndex % m2D_X, m2D_X, p * 0xa511e9b3);
-		uint32 sy = permute(newIndex / m2D_X, m2D_Y, p * 0x63d83595);
-
-#ifndef PR_MJS_USE_RANDOM
-		float jx = randfloat(newIndex, p * 0xa399d265);
-		float jy = randfloat(newIndex, p * 0x711ad6a5);
+#ifdef PR_MJS_CLIP
+		constexpr uint32 FH = 0x51633e2d;
+		constexpr uint32 F1 = 0x68bc21eb;
+		constexpr uint32 F2 = 0x02e5be93;
+		index				= permute(index, maxSamples(), mSeed * FH);
 #else
-		float jx = mRandom.getFloat();
-		float jy = mRandom.getFloat();
+		constexpr uint32 F1 = 0xa511e9b3;
+		constexpr uint32 F2 = 0x63d83595;
 #endif
 
-		auto r = Vector2f((newIndex % m2D_X + (sy + jx) / m2D_Y) / m2D_X,
-						  (newIndex / m2D_X + (sx + jy) / m2D_X) / m2D_Y);
+		const uint32 sx = permute(index % m2D_X, m2D_X, mSeed * F1);
+		const uint32 sy = permute(index / m2D_X, m2D_Y, mSeed * F2);
+
+#ifndef PR_MJS_USE_RANDOM
+		const float jx = randfloat(index, mSeed * 0xa399d265);
+		const float jy = randfloat(index, mSeed * 0x711ad6a5);
+#else
+		const float jx		= mRandom.getFloat();
+		const float jy		= mRandom.getFloat();
+#endif
+
+#ifdef PR_MJS_CLIP
+		const Vector2f r = Vector2f((sx + (sy + jx) / m2D_Y) / m2D_X,
+									(index + jy) / maxSamples());
+#else
+		const Vector2f r	= Vector2f((index % m2D_X + (sy + jx) / m2D_Y) / m2D_X,
+									   (index / m2D_X + (sx + jy) / m2D_X) / m2D_Y);
+#endif
+
 		return r;
 	}
 
 private:
 	Random& mRandom;
 
-	uint32 m2D_X;
-	uint32 m2D_Y;
+	const uint32 m1D;
+	const uint32 m2D_X;
+	const uint32 m2D_Y;
+	const uint32 mSeed;
 };
 
 class MultiJitteredSamplerFactory : public ISamplerFactory {
@@ -156,7 +171,10 @@ public:
 
 	std::shared_ptr<ISampler> createInstance(uint32 sample_count, Random& rnd) const override
 	{
-		return std::make_shared<MultiJitteredSampler>(rnd, sample_count);
+		const uint32 PRIME = 14512081;
+		return std::make_shared<MultiJitteredSampler>(rnd, sample_count,
+													  mParams.getUInt("bins", sample_count),
+													  mParams.getUInt("seed", PRIME ^ rnd.get32()));
 	}
 
 private:
