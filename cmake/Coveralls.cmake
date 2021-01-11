90804c8e8f512d22619427f420cc0846f7d9da0e
Ömercan Yazici <yazici@cg.uni-saarland.de>
2021-01-11 19:07:19 +0100

    Changed coverage

--- cmake/Coveralls.cmake
+++ cmake/Coveralls.cmake
@@ -20,16 +20,12 @@
 # SOFTWARE.
 #
 # Copyright (C) 2014 Joakim Söderberg <joakim.soderberg@gmail.com>
+# Copyright (C) 2021 Ömercan Yazici <omercan@pearcoding.eu>
 #
 
 set(_CMAKE_SCRIPT_PATH ${CMAKE_CURRENT_LIST_DIR}) # must be outside coveralls_setup() to get correct path
 
-#
-# Param _COVERAGE_SRCS	A list of source files that coverage should be collected for.
-# Param _COVERALLS_UPLOAD Upload the result to coveralls?
-#
-
-function(coveralls_setup _COVERAGE_SRCS _COVERALLS_UPLOAD)
+function(coveralls_setup)
 
 	if (ARGC GREATER 2)
 		set(_CMAKE_SCRIPT_PATH ${ARGN})
@@ -40,73 +36,24 @@ function(coveralls_setup _COVERAGE_SRCS _COVERALLS_UPLOAD)
 		message(FATAL_ERROR "Coveralls: Missing ${_CMAKE_SCRIPT_PATH}/CoverallsClear.cmake")
 	endif()
 
-	if (NOT EXISTS "${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake")
-		message(FATAL_ERROR "Coveralls: Missing ${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake")
-	endif()
-
-	# When passing a CMake list to an external process, the list
-	# will be converted from the format "1;2;3" to "1 2 3".
-	# This means the script we're calling won't see it as a list
-	# of sources, but rather just one long path. We remedy this
-	# by replacing ";" with "*" and then reversing that in the script
-	# that we're calling.
-	# http://cmake.3232098.n2.nabble.com/Passing-a-CMake-list-quot-as-is-quot-to-a-custom-target-td6505681.html
-	set(COVERAGE_SRCS_TMP ${_COVERAGE_SRCS})
-	set(COVERAGE_SRCS "")
-	foreach (COVERAGE_SRC ${COVERAGE_SRCS_TMP})
-		set(COVERAGE_SRCS "${COVERAGE_SRCS}*${COVERAGE_SRC}")
-	endforeach()
-
 	#message("Coverage sources: ${COVERAGE_SRCS}")
-	set(COVERALLS_FILE ${PROJECT_BINARY_DIR}/coveralls.json)
+	set(COVERALLS_CMD cpp-coveralls)
 
-	add_custom_target(coveralls_generate
-
+	add_custom_target(coveralls
 		# Zero the coverage counters.
 		COMMAND ${CMAKE_COMMAND} -DPROJECT_BINARY_DIR="${PROJECT_BINARY_DIR}" -P "${_CMAKE_SCRIPT_PATH}/CoverallsClear.cmake"
 
 		# Run regress tests.
 		COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure || true
 
-		# Generate Gcov and translate it into coveralls JSON.
-		# We do this by executing an external CMake script.
-		# (We don't want this to run at CMake generation time, but after compilation and everything has run).
-		COMMAND ${CMAKE_COMMAND}
-				-DCOVERAGE_SRCS="${COVERAGE_SRCS}" # TODO: This is passed like: "a b c", not "a;b;c"
-				-DCOVERALLS_OUTPUT_FILE="${COVERALLS_FILE}"
-				-DCOV_PATH="${PROJECT_BINARY_DIR}"
-				-DPROJECT_ROOT="${PROJECT_SOURCE_DIR}"
-				-P "${_CMAKE_SCRIPT_PATH}/CoverallsGenerateGcov.cmake"
+		# Generate Gcov and upload
+		COMMAND ${COVERALLS_CMD} --root ${PROJECT_SOURCE_DIR} --build-root ${PROJECT_BINARY_DIR} 
+			--include "${PROJECT_SOURCE_DIR}/src/base" --include "${PROJECT_SOURCE_DIR}/src/core"  --include "${PROJECT_SOURCE_DIR}/src/loader" 
+			--gcov-options '\\-lp' -x ".h" -x ".inl" -x ".cpp"
 
 		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-		COMMENT "Generating coveralls output..."
+		COMMENT "Updating coveralls..."
 		)
-
-	if (_COVERALLS_UPLOAD)
-		message("COVERALLS UPLOAD: ON")
-
-		find_program(CURL_EXECUTABLE curl)
-
-		if (NOT CURL_EXECUTABLE)
-			message(FATAL_ERROR "Coveralls: curl not found! Aborting")
-		endif()
-
-		add_custom_target(coveralls_upload
-			# Upload the JSON to coveralls.
-			COMMAND ${CURL_EXECUTABLE}
-					-S -F json_file=@${COVERALLS_FILE}
-					https://coveralls.io/api/v1/jobs
-
-			DEPENDS coveralls_generate
-
-			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-			COMMENT "Uploading coveralls output...")
-
-		add_custom_target(coveralls DEPENDS coveralls_upload)
-	else()
-		message("COVERALLS UPLOAD: OFF")
-		add_custom_target(coveralls DEPENDS coveralls_generate)
-	endif()
 
 endfunction()
 
